a2f17950b481184ef2366a7590b5455d3c7d3e1e
Kuromiya <krmy3993@gmail.com>
2019-07-13 14:16:30 +0900

    修正

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { Table } from "./package/Table";
 
 export { Column } from "./package/decorator/Column";
 export { Model } from "./package/decorator/Model";
+export { AutoIncrementColumn } from "./package/decorator/AutoIncrementColumn";
